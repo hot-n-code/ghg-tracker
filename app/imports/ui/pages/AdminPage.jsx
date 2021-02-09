cda828ed8df0206c517a2968176958a1056b7a03
vrabang <boque.rabang@gmail.com>
2021-02-09 11:23:17 -1000

    finished admin layout page

--- app/imports/ui/pages/AdminPage.jsx
+++ app/imports/ui/pages/AdminPage.jsx
@@ -16,7 +16,20 @@ class AdminPage extends React.Component {
               </Grid.Column>
               <Grid.Column width={10}>
                 <Header as='h1'>Name</Header>
-                <p>Status?Profile Description?Contribution?</p>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Button fluid color='grey'>Edit</Button>
+                <Button fluid color='red'>Remove</Button>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Segment>
+            <Grid>
+              <Grid.Column width={2}>
+                <Image size='small' rounded src='https://images-na.ssl-images-amazon.com/images/I/41LMQ-wZ2oL._AC_SY355_.jpg'/>
+              </Grid.Column>
+              <Grid.Column width={10}>
+                <Header as='h1'>Name</Header>
               </Grid.Column>
               <Grid.Column width={4}>
                 <Button fluid color='grey'>Edit</Button>
