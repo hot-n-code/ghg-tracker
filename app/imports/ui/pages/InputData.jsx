d64e7e1837e97d581420463962246f3ebfe745cc
Daphne Tapia <dmtapia@hawaii.edu>
2021-02-08 20:14:19 -1000

    Update InputData.jsx

--- app/imports/ui/pages/InputData.jsx
+++ app/imports/ui/pages/InputData.jsx
@@ -1,55 +1,62 @@
 import React from 'react';
-import swal from 'sweetalert';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import Meteor from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { Meteor } from 'meteor/meteor';
-import { AutoForm } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import { AutoForm, DateField, ErrorsField, NumField, SelectField, SubmitField } from 'uniforms-semantic';
+import { Data } from '../../api/data/DataCollection';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
 const formSchema = new SimpleSchema({
   date: Date,
   modeOfTransportation: {
     type: String,
-    allowedValues: ['Telework', 'Public Transportation', 'Biking', 'Walking', 'Carpool', 'Electric Vehicle'],
+    allowedValues: ['Alternative Fuel Vehicle', 'Biking', 'Carpool', 'Electric Vehicle', 'Public Transportation', 'Telework', 'Walking'],
   },
-  roundTripMiles: Number,
-  autoMPG: Number,
+  milesTraveled: Number,
 });
 
 const bridge = new SimpleSchema2Bridge(formSchema);
 
-/** Renders the Page for adding a document. */
+/** Renders the Page for inputting data */
 class InputData extends React.Component {
 
-  /** On submit, insert the data and compute GHG. */
+  /** On submit, insert data. */
   submit(data, formRef) {
-    const { date, modeOfTransportation, roundTripMiles, autoMPG } = data;
+    const { date, modeOfTransportation, milesTraveled } = data;
     const owner = Meteor.user().username;
-    GHGDataCollection.collection.insert({ date, modeOfTransportation, roundTripMiles, autoMPG },
+    Data.collection.insert({ date, modeOfTransportation, milesTraveled, owner },
         (error) => {
           if (error) {
-            swal ('Error', error.message, 'error');
+            swal('Error', error.message, 'error');
           } else {
-            swal ('Success', 'Data added and computed successfully', 'success');
+            swal('Success', 'Data added successfully', 'success');
             formRef.reset();
           }
         });
   }
 
-  /** Render the form. */
+  /** Render the form. Uses Uniforms: https://github.com/vazco/uniforms */
   render() {
     let fRef = null;
     return (
         <Grid container centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center">Input New Data</Header>
-            <AutoForm ref={ref = > { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
-              <Segment>
-                <TextField date='date'/>
-              </Segment>
+            <Header as="h2" textAlign="center">Input Data</Header>
+            <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+                <Segment>
+                  <DateField name='date'/>
+                  <SelectField name='modeOfTransportation'/>
+                  <NumField name='milesTraveled'/>
+                  <SubmitField value='Submit'/>
+                  <ErrorsField/>
+                </Segment>
             </AutoForm>
           </Grid.Column>
         </Grid>
-    )
+    );
   }
 }
+
+export default InputData;
