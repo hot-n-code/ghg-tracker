f0f3bd0a7d847720bf6124138d2ba8af191a2cf1
Timt <61216890+TimothyH808@users.noreply.github.com>
2021-02-08 20:53:16 -1000

    update

--- app/imports/ui/pages/CumulativePage.jsx
+++ app/imports/ui/pages/CumulativePage.jsx
@@ -1,47 +1,83 @@
 import React from 'react';
-import { Grid, Header, Image, Container } from 'semantic-ui-react';
+import { Grid, Header, Image, Container, Segment } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class CumulativePage extends React.Component {
   render() {
     return (
-        <Container>
-          <Grid.Column>
-            <div>
-              <Header size='huge' textAlign='center'> Cumulative Data Of Users </Header>
-            </div>
-            <Grid>
-              <Grid.Column width={8}>
-                <Image src="/images/cumulative-page/graph.png"/>
+        <div className='background' >
+          <Container>
+            <Grid.Column>
+              <div>
+                <br/>
+                <Header as='h1' textAlign='center'> Cumulative Data Of Users</Header>
+                <br/>
+              </div>
+              <Grid>
+                <Grid.Column width={9}>
+                  <Image src="/images/cumulative-page/graph.png"/>
+                </Grid.Column>
+                <Grid.Column textAlign='right' width={7}>
+                  <Header as='h1' color='blue'> MODES OF TRAVEL COUNT </Header>
+                  <Header as='h2'>89% OF USERS TELEWORK</Header>
+                  <Header as='h2'>7% OF USERS USE ELECTRIC VEHICLES</Header>
+                  <Header as='h2'>4% OF USERS CARPOOL</Header>
+                  <Header as='h2'>4% OF USERS USE PUBLIC TRANSPORTATION</Header>
+                </Grid.Column>
+              </Grid>
+            </Grid.Column>
+            <Header as='h1' textAlign='center'>
+              ENVIRONMENTAL IMPACT
+              <br/>
+            </Header>
+            <Grid relaxed columns={4}>
+              <Grid.Column>
+                <Segment>
+                  <Image src="/images/cumulative-page/car.png"/>
+                  <Header as='h1' dividing textAlign='center'>
+                    Vehicle Miles Travel Reduced
+                  </Header>
+                  <Header as='h2' textAlign='center' block>
+                    4,298 MILES
+                  </Header>
+                </Segment>
               </Grid.Column>
-              <Grid.Column textAlign='right' width={8}>
-                <Header as='h1'>MODES OF TRAVEL COUNT</Header>
+              <Grid.Column>
+                <Segment>
+                  <Image src="/images/cumulative-page/C.png"/>
+                  <Header as='h1' dividing textAlign='center'>
+                    Green House Gas (GHG) Reduced
+                  </Header>
+                  <Header as='h2' textAlign='center' block>
+                    2,176 POUNDS
+                  </Header>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment>
+                  <Image src="/images/cumulative-page/gas.png"/>
+                  <Header as='h1' dividing textAlign='center'>
+                    Gallons of Gas Saved
+                  </Header>
+                  <Header as='h2' textAlign='center' block>
+                    121 GALLONS
+                  </Header>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment>
+                  <Image src="/images/cumulative-page/bike.png"/>
+                  <Header as='h1' dividing textAlign='center'>
+                    Days Biked to Work
+                  </Header>
+                  <Header as='h2' textAlign='center' block>
+                    10 DAYS
+                  </Header>
+                </Segment>
               </Grid.Column>
             </Grid>
-            <Grid>
-              <Grid.Column textAlign='left' width={8}>
-                <Header as='h1'>MODES OF TRAVEL COUNT</Header>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Image src="/images/cumulative-page/graph2.png"/>
-              </Grid.Column>
-            </Grid>
-          </Grid.Column>
-          <Grid relaxed columns={4}>
-            <Grid.Column>
-              <Image src="/images/cumulative-page/car.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/cumulative-page/C.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/cumulative-page/gas.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/cumulative-page/bike.png"/>
-            </Grid.Column>
-          </Grid>
-        </Container>
+          </Container>
+        </div>
     );
   }
 }
