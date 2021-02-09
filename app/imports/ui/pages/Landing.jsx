64aef1f0b2099d5e0924afce39f75b6440f85037
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-02-09 10:12:38 -0800

    Nav-bar, landing page, fonts, rough

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
+
+const menuStyle = { height: '500px' };
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        <div>
+          <div className='background-landing' style={menuStyle}>
+            <div className='ghg-text'>
+            <Header textAlign="center" size="massive" inverted> GHG Tracker </Header>
+            </div>
+            <Header className="logo-description" textAlign="center" inverted> Tracking greenhouse gas emissions that are emitted from your every day transportation.</Header>
+          </div>
+        </div>
     );
   }
 }
