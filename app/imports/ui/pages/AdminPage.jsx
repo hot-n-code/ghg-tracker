e8545d8824a9d0491ae269c6d8a74662270117c3
vrabang <boque.rabang@gmail.com>
2021-02-08 20:52:12 -1000

    AdminPage layout

--- app/imports/ui/pages/AdminPage.jsx
+++ app/imports/ui/pages/AdminPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Input, Container, Grid, Segment, Header } from 'semantic-ui-react';
+import { Input, Container, Image, Header, Grid, Segment, Button } from 'semantic-ui-react';
 
 class AdminPage extends React.Component {
   render() {
@@ -7,6 +7,23 @@ class AdminPage extends React.Component {
         <Container>
           <Input fluid icon='search' placeholder='Search...'>
           </Input>
+          <br/>
+          <br/>
+          <Segment>
+            <Grid>
+              <Grid.Column width={2}>
+                <Image size='small' rounded src='https://images-na.ssl-images-amazon.com/images/I/41LMQ-wZ2oL._AC_SY355_.jpg'/>
+              </Grid.Column>
+              <Grid.Column width={10}>
+                <Header as='h1'>Name</Header>
+                <p>Status?Profile Description?Contribution?</p>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Button fluid color='grey'>Edit</Button>
+                <Button fluid color='red'>Remove</Button>
+              </Grid.Column>
+            </Grid>
+          </Segment>
         </Container>
     );
   }
