01cf4dc7e1f7c3e7cca545251e6a3e9ee95e9d53
Jackie Wong <wongjackie888@gmail.com>
2021-02-09 01:12:22 -1000

    Create general layout of the 'My Vehicles' page.
    - Add sample vehicle data.
    - Render semantic-ui 'Card' component in 'VehicleCard' component.
    - Create general layout of 'My Vehicles' page in the 'MyVehicles' component without styling semantic-ui components.
    - Render 'VehicleCard' in 'MyVehicles' component.

--- app/imports/ui/components/VehicleCard.jsx
+++ app/imports/ui/components/VehicleCard.jsx
@@ -3,16 +3,26 @@ import { Card } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
-/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+/** Renders a single vehicle card. */
 class VehicleCard extends React.Component {
   render() {
     return (
       <Card centered>
         <Card.Content>
           <h1>BRAND LOGO</h1>
-          <Card.Header>BRAND NAME</Card.Header>
-          <Card.Meta>MODEL</Card.Meta>
-          <Card.Description>Avg. Fuel Consumption: 25 MPG</Card.Description>
+          <Card.Header>{this.props.vehicle.make}</Card.Header>
+          <Card.Meta>
+            {this.props.vehicle.year} {this.props.vehicle.model}
+          </Card.Meta>
+          <Card.Description>
+            Purchase Price: {this.props.vehicle.price}
+          </Card.Description>
+          <Card.Description>
+            Average Fuel Consumption: {this.props.vehicle.consumption}
+          </Card.Description>
+          <Card.Description>
+            Yearly Fuel Spending: {this.props.vehicle.fuelSpending}
+          </Card.Description>
         </Card.Content>
         <Card.Content extra>Show More</Card.Content>
       </Card>
@@ -22,7 +32,7 @@ class VehicleCard extends React.Component {
 
 /** Require a document to be passed to this component. */
 VehicleCard.propTypes = {
-  stuff: PropTypes.object.isRequired,
+  vehicle: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
--- app/imports/ui/pages/MyVehicles.jsx
+++ app/imports/ui/pages/MyVehicles.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader } from 'semantic-ui-react';
+import { Container, Header, Loader, Search } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Stuffs } from '../../api/stuff/Stuff';
+import VehicleCard from '../components/VehicleCard';
 
-/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+// Create sample placeholder vehicle data for mock-up
+const sampleVehicleData = [
+  {
+    make: 'Toyota',
+    model: 'Corolla',
+    price: '22,000',
+    year: '2016',
+    consumption: '24 MPG',
+    fuelSpending: '1800',
+  },
+  {
+    make: 'Honda',
+    model: 'Civic',
+    price: '22,000',
+    year: '2018',
+    consumption: '26 MPG',
+    fuelSpending: '1700',
+  },
+  {
+    make: 'Nissan',
+    model: 'Sentra',
+    price: '22,000',
+    year: '2019',
+    consumption: '26 MPG',
+    fuelSpending: '1600',
+  },
+];
+
+/** Renders a feed containing all of the Vehicle documents. Use <VehicleCard> to render each card. */
 class MyVehicles extends React.Component {
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -23,23 +52,27 @@ class MyVehicles extends React.Component {
         <Header as='h2' textAlign='center'>
           My Vehicles
         </Header>
+        <Search input={{ icon: 'search', iconPosition: 'left' }} />
+        {sampleVehicleData.map((vehicle, index) => (
+          <VehicleCard vehicle={vehicle} key={index} />
+        ))}
       </Container>
     );
   }
 }
 
-/** Require an array of Stuff documents in the props. */
+/** Require an array of Vehicle documents in the props. */
 MyVehicles.propTypes = {
-  stuffs: PropTypes.array.isRequired,
+  // KEEP FOR REFERENCE: stuffs: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  // Get access to Stuff documents.
+  // KEEP FOR REFERENCE: Get access to Stuff documents.
   const subscription = Meteor.subscribe(Stuffs.userPublicationName);
   return {
-    stuffs: Stuffs.collection.find({}).fetch(),
+    // KEEP FOR REFERENCE: stuffs: Stuffs.collection.find({}).fetch(),
     ready: subscription.ready(),
   };
 })(MyVehicles);
