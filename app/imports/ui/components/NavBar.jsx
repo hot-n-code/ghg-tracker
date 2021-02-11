5d557970a33f41b6c27ee319613af5ab374a2856
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-02-11 02:12:49 -0800

    Revert "Merge branch 'issue-10'"
    This reverts commit 431b1505d90ff6c7d28ede746afcfd998a6f26d1, reversing
    changes made to 58b552ee1de95d22a0680ea4ecf52ac8ac8c647c.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -89,141 +89,65 @@ class NavBar extends React.Component {
               {' '}
               {/* zero padding!!! */}
               <Menu secondary stackable id='right-nav-bar'>
-        <Menu style={menuStyle} attached='top' borderless stackable>
-          <Menu.Item as={NavLink} activeClassName='' exact to='/'>
-            <Image src={cornerLogo} size='small' padding={0} />
-          </Menu.Item>
-          {this.props.currentUser && !Roles.userIsInRole(Meteor.userId(), 'admin')
-              ? [
                 <Menu.Item
-                    as={NavLink}
-                    activeClassName='active'
-                    exact
-                    to='/my-vehicles'
-                    key='my-vehicles'
-                >
-                  My Vehicles
-                </Menu.Item>,
-                <Menu.Item
-                    as={NavLink}
-                    activeClassName='active'
-                    exact
-                    to='/userPage'
-                    key='about'
-                >
-                  My Data
-                </Menu.Item>,
-                <Menu.Item
-                    as={NavLink}
-                    activeClassName='active'
-                    exact
-                    to='/inputdailydata'
-                    key='inputdailydata'
-                >
-                  Input Data
-                </Menu.Item>,
-                <Menu.Item
-                    as={NavLink}
-                    activeClassName='active'
-                    exact
-                    to='/feedback'
-                    key='add'
-                >
-                  Feedback
-                </Menu.Item>,
-              ]
-              : ''}
-          {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Menu.Item
                   as={NavLink}
                   activeClassName='active'
                   exact
-                  to='/admin'
-                  key='admin'
-              >
-                User List
-              </Menu.Item>
-          ) : (
-              ''
-          )}
-          {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Menu.Item
-                  as={NavLink}
-                  activeClassName='active'
-                  exact
-                  to='/admindata'
-                  key='admin'> Admin
-                <br />
-                Cumulative Data
-              </Menu.Item>
-          ) : (
-              ''
-          )}
-          <Menu.Item position='right'>
-            {this.props.currentUser === '' ? (
-                <Menu.Item position='right'>
-                  {' '}
-                  {/* zero padding!!! */}
-                  <Menu secondary stackable id='right-nav-bar'>
-                    <Menu.Item
-                        as={NavLink}
-                        activeClassName='active'
-                        exact
-                        to='/cumulative'
-                        key='cumulative'
-                    >
-                      CUMULATIVE
-                      <br />
-                      DATA <br />
-                    </Menu.Item>
-                    <Menu.Item
-                        as={NavLink}
-                        activeClassName='active'
-                        exact
-                        to='/about'
-                        key='about'
-                    >
-                      ABOUT HEI
-                    </Menu.Item>
-                    <Menu.Item
-                        id='login-dropdown-sign-up'
-                        name='Join'
-                        as={NavLink}
-                        exact
-                        to='/signup'
-                        content='JOIN'
-                    />
-                    <Menu.Item
-                        id='login-dropdown-sign-in'
-                        name='Signin'
-                        as={NavLink}
-                        exact
-                        to='/signin'
-                        content='SIGN IN'
-                    />
-                  </Menu>
+                  to='/cumulative'
+                  key='cumulative'
+                >
+                  CUMULATIVE
+                  <br />
+                  DATA <br />
                 </Menu.Item>
-            ) : (
-                <Dropdown
-                    id='navbar-current-user'
-                    text={this.props.currentUser}
-                    pointing='top right'
-                    icon={'user'}
+                <Menu.Item
+                  as={NavLink}
+                  activeClassName='active'
+                  exact
+                  to='/about'
+                  key='about'
                 >
-                  <Dropdown.Menu>
-                    <Dropdown.Item
-                        id='navbar-sign-out'
-                        icon='sign out'
-                        text='Sign Out'
-                        as={NavLink}
-                        exact
-                        to='/signout'
-                    />
-                  </Dropdown.Menu>
-                </Dropdown>
-            )}
-          </Menu.Item>
-        </Menu>
+                  ABOUT HEI
+                </Menu.Item>
+                <Menu.Item
+                  id='login-dropdown-sign-up'
+                  name='Join'
+                  as={NavLink}
+                  exact
+                  to='/signup'
+                  content='JOIN'
+                />
+                <Menu.Item
+                  id='login-dropdown-sign-in'
+                  name='Signin'
+                  as={NavLink}
+                  exact
+                  to='/signin'
+                  content='SIGN IN'
+                />
+              </Menu>
+            </Menu.Item>
+          ) : (
+            <Dropdown
+              id='navbar-current-user'
+              text={this.props.currentUser}
+              pointing='top right'
+              icon={'user'}
+            >
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  id='navbar-sign-out'
+                  icon='sign out'
+                  text='Sign Out'
+                  as={NavLink}
+                  exact
+                  to='/signout'
+                />
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
+        </Menu.Item>
+      </Menu>
     );
   }
 }
