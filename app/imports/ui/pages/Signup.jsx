b93a4704c0db63f47a8a6f3f2f2fe77b220b27d5
micgainey <45333969+micgainey@users.noreply.github.com>
2021-02-10 19:05:02 -1000

    fixed lint error

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -43,7 +43,7 @@ class Signup extends React.Component {
           <div className='signup'>
       <Grid centered container className='signup-page'>
         <Grid.Column>
-          <div style={{ marginRight: 'auto', marginLeft: 'auto', paddingBottom: '40px', paddingTop: '40px', width: '800px'}}>
+          <div style={{ marginRight: 'auto', marginLeft: 'auto', paddingBottom: '40px', paddingTop: '40px', width: '800px' }}>
             <Header as='h2' textAlign='center'>Start tracking your greenhouse gas emissions!</Header>
             <Segment padded color='gray'>
               <Form onSubmit={this.submit}>
