6e463f813c2d0f981fb9130d6ec5ec75116d9afb
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-02-11 11:08:46 -0800

    New Landing Branch

--- app/imports/ui/components/LandingCumulativeData.jsx
+++ app/imports/ui/components/LandingCumulativeData.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Grid, Image, Button, Segment } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 const paddingStyle = { padding: '20px' };
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
-class LandingCumulativeData extends React.Component {
-  render() {
-    return (
-        <div className='landing-data'>
-        <Header as='h1' style={paddingStyle}>Cumulative Data in Hawaii</Header>
-        </div>
+const LandingCumulativeData = () => (
+    <div className='landing-data' style={paddingStyle}>
+      <Header as='h1' >Cumulative Data in Hawaii</Header>
+      <Grid columns={2} stackable >
+        <Grid.Column>
+          <Image src='/images/cumulative-page/graph.png'></Image>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment color='grey' > <Header as='h1'> Carbon Saved to Date: </Header>
+            <Image src='/images/landing-page/temp-graph.png' size='large'></Image>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+      <Header as='h1' textAlign='center'> <u>Malama I Ka `Aina</u></Header>
+      <Header as='h2' textAlign='center'> Do your part to save Hawai&apos;i and track your carbon footprint today.
+        <br/>
+        <Button className="ui massive black basic button" as={NavLink} activeClassName="active" exact to="/signup" >Sign Up</Button>
+      </Header>
+    </div>
     );
-  }
-}
 
 export default LandingCumulativeData;
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,73 +1,27 @@
 import React from 'react';
-import { Header, Grid, Embed } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
 import LandingNewsFeed from '../components/LandingNewsFeed';
 import LandingCumulativeData from '../components/LandingCumulativeData';
+import LandingDescription from '../components/LandingDescription';
 
 const menuStyle = { height: '500px' };
-const paddingStyle = { padding: '20px' };
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <div>
-        <div className='background-landing' style={menuStyle}>
-          <div className='ghg-text'>
-            <Header textAlign='center' size='massive' inverted>
-              {' '}
-              GHG Tracker{' '}
-            </Header>
-          </div>
-          <Header className='logo-description' textAlign='center' inverted>
-            {' '}
-            Tracking greenhouse gas emissions that are released from your every
-            day transportation.
-          </Header>
-        </div>
         <div>
-          <Grid columns={2} stackable style={paddingStyle}>
-            <Grid.Column>
-              <Header as='h1' textAlign='center'>
-                {' '}
-                <u>WHO ARE WE?</u>{' '}
-              </Header>
-              <br />
-              <Header as='h2' textAlign='center'>
-                We want to help the people of Hawaii track their carbon
-                footprint.
-                <br />
-                Climate change has the potential to increase the severity and
-                frequency of hurricanes, flooding, and droughts, and is expected
-                to lead to increasing sea level rise.
-                <br />
-                These climate change impacts could cause damage to our physical
-                facilities at Hawaiian Electric and to the properties that
-                secure American Savings Bank’s residential and commercial loan
-                <br />
-                GHG is the most significant factor in climate change.
-              </Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header as='h2'>
-                Learn about the impact of cars carbon emission on the planet
-              </Header>
-              <Embed
-                id='r-qZ4qxLgDc'
-                placeholder='images/video-prev.png'
-                source='youtube'
-              />
-              <Header as='h3'>
-                <i>
-                  Source: Cars (CISL Version). YouTube, uploaded by Real World
-                  Visuals.
-                </i>
-              </Header>
-            </Grid.Column>
-          </Grid>
+          <div className='background-landing' style={menuStyle}>
+            <div className='ghg-text'>
+              <Header textAlign="center" size="massive" inverted> GHG Tracker </Header>
+            </div>
+            <Header className="logo-description" textAlign="center" inverted> Tracking greenhouse gas emissions that are released from your every day transportation.</Header>
+            <Image src='/images/landing-page/black-trans.png' size='large' centered ></Image>
+          </div>
+          <LandingDescription/>
+          <LandingCumulativeData/>
+          <LandingNewsFeed />
         </div>
-        <LandingCumulativeData />
-        <LandingNewsFeed />
-      </div>
     );
   }
 }
