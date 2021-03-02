2efc83834234fdbefc98c53e4fba04313ec8d580
Jackie Wong <wongzjackie@gmail.com>
2021-03-02 00:38:20 -1000

    Update 'MyVehicles' component and separate vehicle list to another component.

--- app/imports/ui/pages/MyVehicles.jsx
+++ app/imports/ui/pages/MyVehicles.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Button, Loader } from 'semantic-ui-react';
+import { Button, Grid, Header, Search, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
-// import { motion } from 'framer-motion';
+// import { _ } from 'meteor/underscore';
 import PropTypes from 'prop-types';
+// import VehicleCard from '../components/VehicleCard';
 import VehicleList from '../components/VehicleList';
 import { Vehicle } from '../../api/vehicle/VehicleCollection';
 import { Make } from '../../api/make/Make';
@@ -22,12 +23,11 @@ class MyVehicles extends React.Component {
   /** Render the page once subscriptions of Vehicles collection have been received. */
   renderPage() {
     /** Finds the vehicles that are owned by the user */
-    const email = Meteor.user().username;
-    const userVehicles = Vehicle.collection.find({ owner: email }).fetch();
+    // const email = Meteor.user().username;
+    // const userVehicles = Vehicle.collection.find({ owner: email }).fetch();
     return (
-      <div className='background-all'>
-        <VehicleList userVehicles={userVehicles} />
-        {/* <Grid centered stackable columns={1} className={'my-vehicles-grid'}>
+      <div className='vehicle-list-container background-all'>
+        <Grid centered stackable columns={1} className={'my-vehicles-grid'}>
           <Grid.Column>
             <Header as='h1' textAlign='center'>
               My Vehicles
@@ -40,15 +40,16 @@ class MyVehicles extends React.Component {
             />
           </Grid.Column>
           <Grid.Column>
-            <Grid stackable columns={3}>
+            {/* <Grid stackable columns={3}>
               {_.map(userVehicles, (vehicle, index) => (
                 <Grid.Column key={index}>
                   <VehicleCard vehicle={vehicle} />
                 </Grid.Column>
               ))}
-            </Grid>
+            </Grid> */}
+            <VehicleList />
           </Grid.Column>
-        </Grid> */}
+        </Grid>
         <a href='#/create-vehicle'>
           <Button circular icon='add' size='massive' />
         </a>
