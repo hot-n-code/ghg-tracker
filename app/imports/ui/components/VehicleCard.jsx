8e3b31292d64b17057a04c7dfc96b8e50fe84df1
Jackie Wong <wongzjackie@gmail.com>
2021-03-02 00:37:00 -1000

    Update VehicleCard component.
    - Add initial animation for card modal pop up.

--- app/imports/ui/components/VehicleCard.jsx
+++ app/imports/ui/components/VehicleCard.jsx
@@ -1,76 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 
 /** Renders a single vehicle card. */
-class VehicleCard extends React.Component {
-  render() {
-    const {
-      year,
-      make,
-      model,
-      logo,
-      price,
-      MPG,
-      fuelSpending,
-    } = this.props.vehicle;
-    const { id } = this.props.id;
-    return (
-      <Link className='vehicle-card-link' to={id}>
-        <div className='vehicle-card'>
-          <div className='vehicle-card-container'>
+const VehicleCard = ({ vehicle }) => {
+  // State
+  const [selectedId, setSelectedId] = useState(null);
+  // Destructure vehicle prop object.
+  const { _id, year, make, model, logo, price, MPG, fuelSpending } = vehicle;
+
+  return (
+    <AnimateSharedLayout>
+      <motion.div
+        className='vehicle-card'
+        layoutId={_id}
+        onClick={() => setSelectedId(_id)}
+      >
+        <div className='vehicle-card-container'>
+          <motion.div
+            className='vehicle-card-content'
+            layoutId={`vehicle-card-container-${_id}`}
+          >
             <motion.div
-              className='vehicle-card-content'
-              layoutId={`vehicle-card-container-${id}`}
+              className='vehicle-card-logo'
+              layoutId={`vehicle-card-logo-${_id}`}
             >
-              <motion.div
-                className='vehicle-card-logo'
-                layoutId={`vehicle-card-logo-${id}`}
-              >
-                <img src={logo} alt={`${make} Logo`} />
-              </motion.div>
-              <motion.div
-                className='vehicle-card-header'
-                layoutId={`vehicle-card-header-${id}`}
-              >
-                {`${year} {make} {model}`}
-              </motion.div>
-              <motion.div
-                className='vehicle-card-description'
-                layoutId={`vehicle-card-price-${id}`}
-              >
-                <span className='vehicle-card-label'>Purchase Price: </span>$
-                {`${price}`}
-              </motion.div>
-              <motion.div
-                className='vehicle-card-description'
-                layoutId={`vehicle-card-consumption-${id}`}
-              >
-                <span className='vehicle-card-label'>
-                  Average Fuel Consumption:{' '}
-                </span>
-                {`${MPG}`}
-              </motion.div>
-              <motion.div
-                className='vehicle-card-description'
-                layoutId={`vehicle-card-spending-${id}`}
-              >
-                <span className='vehicle-card-label'>
-                  Yearly Fuel Spending:{' '}
-                </span>
-                {`${fuelSpending}`}
+              <img src={logo} alt={`${make} Logo`} />
+            </motion.div>
+            <motion.div
+              className='vehicle-card-header'
+              layoutId={`vehicle-card-header-${_id}`}
+            >
+              {`${year} ${make} ${model}`}
+            </motion.div>
+            <motion.div
+              className='vehicle-card-description'
+              layoutId={`vehicle-card-price-${_id}`}
+            >
+              <span className='vehicle-card-label'>Purchase Price: </span>$
+              {`${price}`}
+            </motion.div>
+            <motion.div
+              className='vehicle-card-description'
+              layoutId={`vehicle-card-consumption-${_id}`}
+            >
+              <span className='vehicle-card-label'>
+                Average Fuel Consumption:{' '}
+              </span>
+              {`${MPG}`}
+            </motion.div>
+            <motion.div
+              className='vehicle-card-description'
+              layoutId={`vehicle-card-spending-${_id}`}
+            >
+              <span className='vehicle-card-label'>Yearly Fuel Spending: </span>
+              {`${fuelSpending}`}
+            </motion.div>
+          </motion.div>
+        </div>
+      </motion.div>
+
+      <AnimatePresence>
+        {selectedId && (
+          <motion.div className='vehicle-card-expand-overlay'>
+            <motion.div
+              className='vehicle-card-expand'
+              layoutId={selectedId}
+              onClick={() => setSelectedId(null)}
+            >
+              <motion.div className='vehicle-card-expand-container'>
+                <motion.div className='vehicle-card-expand-content'>
+                  <motion.div className='vehicle-card-expand-logo'>
+                    <img src={logo} alt={`${make} Logo`} />
+                  </motion.div>
+                  <motion.div className='vehicle-card-expand-header'>
+                    {`${year} ${make} ${model}`}
+                  </motion.div>
+                  <motion.div className='vehicle-card-expand-description'>
+                    <span className='vehicle-card-label'>Purchase Price: </span>
+                    ${`${price}`}
+                  </motion.div>
+                  <motion.div className='vehicle-card-expand-description'>
+                    <span className='vehicle-card-label'>
+                      Average Fuel Consumption:{' '}
+                    </span>
+                    {`${MPG}`}
+                  </motion.div>
+                  <motion.div className='vehicle-card-expand-description'>
+                    <span className='vehicle-card-label'>
+                      Yearly Fuel Spending:{' '}
+                    </span>
+                    {`${fuelSpending}`}
+                  </motion.div>
+                </motion.div>
               </motion.div>
             </motion.div>
-          </div>
-        </div>
-      </Link>
-    );
-  }
-}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </AnimateSharedLayout>
+  );
+};
 
 /** Currently, placeholder vehicle data is passed to this component. In production, require a document to be passed to this component. */
 VehicleCard.propTypes = {
   vehicle: PropTypes.object.isRequired,
-  id: PropTypes.string.isRequired,
 };
+
+export default VehicleCard;
