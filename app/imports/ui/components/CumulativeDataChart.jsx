daa42c7253b409819c1c41c42a3589fd0381cb7b
Timt <timhuo808@gmail.com>
2021-03-03 22:50:12 -1000

    Update CumulativeDataChart page

--- app/imports/ui/components/CumulativeDataChart.jsx
+++ app/imports/ui/components/CumulativeDataChart.jsx
@@ -15,52 +15,48 @@ class CumulativeDataChart extends React.Component {
     };
   }
 
-  render() {
-    const percentage = (num, total) => ((num / total) * 100).toFixed(1);
-
-    const transportationData = (dailyUser) => {
-      const altTransportation = ['Biking', 'Public Transportation', 'Walking'];
-      const allModeData = _.pluck(dailyUser, 'modeOfTransportation');
-      const altData = {
-        Telework: 0,
-        Carpool: 0,
-        Other: 0,
-        Vehicle: 0,
-      };
-      allModeData.map((mode) => {
-        if (mode === 'Telework') {
-          altData.Telework += 1;
-        } else if (mode === 'Carpool') {
-          altData.Carpool += 1;
-        } else if (altTransportation.includes(mode)) {
-          altData.Other += 1;
-        } else {
-          altData.Vehicle += 1;
-        }
-        return altData;
-      });
-      return [altData.Telework, altData.Carpool, altData.Other, altData.Vehicle];
+  transportationData = (dailyUser) => {
+    const altTransportation = ['Biking', 'Public Transportation', 'Walking'];
+    const allModeData = _.pluck(dailyUser, 'modeOfTransportation');
+    const altData = {
+      Telework: 0,
+      Carpool: 0,
+      Other: 0,
+      Vehicle: 0,
     };
+    allModeData.map((mode) => {
+      if (mode === 'Telework') {
+        altData.Telework += 1;
+      } else if (mode === 'Carpool') {
+        altData.Carpool += 1;
+      } else if (altTransportation.includes(mode)) {
+        altData.Other += 1;
+      } else {
+        altData.Vehicle += 1;
+      }
+      return altData;
+    });
+    return [altData.Telework, altData.Carpool, altData.Other, altData.Vehicle];
+  };
 
-    const pieData = transportationData(this.props.dailyUserData);
-    const totalTransportation = _.reduce(pieData, (memo, num) => memo + num);
+  pieDataSet = (dailyUser) => {
     const dataSets = [
       {
-        data: pieData,
+        data: this.transportationData(dailyUser),
         backgroundColor: ['#4f7fa0', '#4b8796', '#6872a0', '#846391'],
       },
     ];
+    return dataSets;
+  }
+
+  render() {
     return (
         <Grid>
           <Grid.Column width={9}>
-            <Pie data={{ labels: this.state.labels, datasets: dataSets }} height='200px'/>
+            <Pie data={{ labels: this.state.labels, datasets: this.pieDataSet(this.props.dailyUserData) }} height='200px'/>
           </Grid.Column>
           <Grid.Column textAlign='right' width={7}>
             <Header as='h1' color='blue'> MODES OF TRAVEL COUNT </Header>
-            <Header as='h2'>{percentage(pieData[0], totalTransportation)}% OF USERS TELEWORK</Header>
-            <Header as='h2'>{percentage(pieData[1], totalTransportation)}7% OF USERS USE CARPOOL</Header>
-            <Header as='h2'>{percentage(pieData[2], totalTransportation)}4% OF USERS USE OTHER ALTERNATIVE TRANSPORTATION</Header>
-            <Header as='h2'>{percentage(pieData[3], totalTransportation)}4% OF USERS USE VEHICLES</Header>
           </Grid.Column>
         </Grid>
     );
