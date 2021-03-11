92cc24db1b0fa7ac0f614bf00522d1413dc5a370
vrabang <boque.rabang@gmail.com>
2021-03-11 09:27:46 -1000

    fixed eslint issue

--- app/imports/ui/pages/AdminUserList.jsx
+++ app/imports/ui/pages/AdminUserList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Loader, Table, Container, Input, Button } from 'semantic-ui-react';
+import { Loader, Table, Container, Input } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
