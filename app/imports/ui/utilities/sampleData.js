e7935c96e62c6f2edb635790d872cff34fb52f12
Jackie Wong <wongzjackie@gmail.com>
2021-03-10 18:50:53 -1000

    Create sample data for alternative energy vehicles.

---
+++ app/imports/ui/utilities/sampleData.js
@@ -0,0 +1,33 @@
+export const sampleVehicles = [
+  {
+    make: 'Tesla',
+    model: 'Model 3',
+    logo: 'https://www.freepnglogos.com/uploads/tesla-logo-png-black-1.png',
+    price: 30000,
+    year: 2019,
+    MPG: 50,
+    fuelSpending: 0,
+    type: 'Electric',
+  },
+  {
+    make: 'Nissan',
+    model: 'Leaf',
+    logo: 'https://www.freepnglogos.com/uploads/nissan-logo-23.png',
+    price: 24000,
+    year: 2018,
+    MPG: 35,
+    fuelSpending: 800,
+    type: 'Hybrid',
+  },
+  {
+    make: 'Toyota',
+    model: 'Prius',
+    logo:
+      'https://www.freepnglogos.com/uploads/toyota-logo-png/toyota-logos-download-image-15.png',
+    price: 25000,
+    year: 2019,
+    MPG: 53,
+    fuelSpending: 700,
+    type: 'Hybrid',
+  },
+];
