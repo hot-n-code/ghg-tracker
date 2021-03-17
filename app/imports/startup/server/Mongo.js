fc8788129b16c6c697704a3f1a99af3b34b33f2e
Daphne Tapia <dmtapia@hawaii.edu>
2021-03-16 22:16:18 -1000

    Update Mongo.js
    organized, trying to fix publications

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -10,12 +10,46 @@ import { AllVehicle } from '../../api/vehicle/AllVehicleCollection';
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
+// StuffsCollection
 function addData(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.collection.insert(data);
 }
 
+// DailyUserDataCollection
+function addDailyUserData(dailyData) {
+  console.log(`  Defining Daily User Data on: ${dailyData.inputDate}`);
+  DailyUserData.collection.insert(dailyData);
+}
+
+// VehicleCollection
+function addVehicle(vehicle) {
+  console.log(`  Defining vehicle ${vehicle.owner}`);
+  Vehicle.collection.insert(vehicle);
+}
+
+// UserCollection
+function addUser({ name, goal, email, image, vehicles }) {
+  console.log(`  Defining profile ${email}`);
+  Users.collection.insert({ name, goal, email, image });
+  // Add interests and projects.
+  vehicles.map(vehicle => UserVehicle.collection.insert({ user: email, model: vehicle }));
+}
+
+// MakeCollection
+function addMake(data) {
+  console.log(`  Defining make ${data.make}`);
+  Make.collection.insert(data);
+}
+
+// AllVehicleCollection
+function addAllVehicle(vehicle) {
+  console.log(`  Defining all vehicle ${vehicle.make}`);
+  AllVehicle.collection.insert(vehicle);
+}
+
 /** Initialize the collection if empty. */
+// StuffsCollection
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
@@ -23,47 +57,15 @@ if (Stuffs.collection.find().count() === 0) {
   }
 }
 
-/** Initialize the database with a default data document. */
-function addDailyUserData({ owner, inputDate, modeOfTransportation, milesTraveled, cO2Reduced }) {
-  console.log(`  Defining Daily User Data (${owner})`);
-  DailyUserData.collection.insert({ owner, inputDate, modeOfTransportation, milesTraveled, cO2Reduced });
-}
-
-/** Initialize the collection if empty. */
+// DailyUserDataCollection
 if (DailyUserData.collection.find().count() === 0) {
   if (Meteor.settings.defaultDailyUserData) {
     console.log('Creating default data.');
-    Meteor.settings.defaultDailyUserData.map(userData => addDailyUserData(userData));
+    Meteor.settings.defaultDailyUserData.map(dailyData => addDailyUserData(dailyData));
   }
 }
 
-/** Initialize the database with a default data document. */
-function addVehicle({
-  make,
-  model,
-  owner,
-  logo,
-  price,
-  year,
-  MPG,
-  fuelSpending,
-  type,
-}) {
-  console.log(`Defining vehicle ${owner}`);
-  Vehicle.collection.insert({
-    make,
-    model,
-    owner,
-    logo,
-    price,
-    year,
-    MPG,
-    fuelSpending,
-    type,
-  });
-}
-
-/** Initialize the collection if empty. */
+// VehicleCollection
 if (Vehicle.collection.find().count() === 0) {
   if (Meteor.settings.defaultVehicle) {
     console.log('Creating default Vehicle.');
@@ -71,46 +73,23 @@ if (Vehicle.collection.find().count() === 0) {
   }
 }
 
-/** Initialize the database with a default data document. */
-function addUser({ name, goal, email, image, vehicles }) {
-  console.log(`Defining profile ${email}`);
-  Users.collection.insert({ name, goal, email, image });
-  // Add interests and projects.
-  vehicles.map(vehicle => UserVehicle.collection.insert({ user: email, model: vehicle }));
-}
-
-/** Initialize the collection if empty. */
+// UserCollection
 if (Users.collection.find().count() === 0) {
   if (Meteor.settings.defaultUser) {
     console.log('Creating the default profiles');
-    Meteor.settings.defaultUser.map(user => addUser(user));
-  } else {
-    console.log(
-      'Cannot initialize the database!  Please invoke meteor with a settings file.',
-    );
+    Meteor.settings.defaultUser.map(individualUser => addUser(individualUser));
   }
 }
 
-function addMake(data) {
-  console.log(`Defining make ${data.make}`);
-  Make.collection.insert(data);
-}
-
-/** Initialize the collection if empty. */
+// MakeCollection
 if (Make.collection.find().count() === 0) {
   if (Meteor.settings.defaultMakes) {
     console.log('Creating default make.');
-    Meteor.settings.defaultMakes.map(data => addMake(data));
+    Meteor.settings.defaultMakes.map(makes => addMake(makes));
   }
 }
 
-/** Initialize the database with a default data document. */
-function addAllVehicle({ Year, MMake, Model, Mpg }) {
-  console.log(`Defining all vehicle ${MMake}`);
-  AllVehicle.collection.insert({ Year, MMake, Model, Mpg });
-}
-
-/** Initialize the collection if empty. */
+// AllVehicleCollection
 if (AllVehicle.collection.find().count() === 0) {
   if (Meteor.settings.Vehicles) {
     console.log('Creating default Vehicle.');
