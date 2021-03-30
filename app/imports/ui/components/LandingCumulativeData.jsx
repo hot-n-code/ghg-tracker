b9f447d10ef5278898529e4d707c59e1ad74f0ad
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-03-29 18:11:26 -0700

    cloud divider

--- app/imports/ui/components/LandingCumulativeData.jsx
+++ app/imports/ui/components/LandingCumulativeData.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Header, Button, Image } from 'semantic-ui-react';
+import { Header, Button, Image, Grid } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 const paddingStyle = { padding: '20px' };
+const cloud = '/images/landing-page/cloud-trans-2.png';
+
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const LandingCumulativeData = () => (
     <div className='landing-data' style={paddingStyle}>
       <Header as='h1' textAlign='center'> <u>Malama I Ka `Aina</u></Header>
       <Header as='h2' textAlign='center'> Do your part to save Hawai&apos;i and track your carbon footprint today. </Header>
-      <Image centered src='/images/landing-page/cloud-trans-2.png' padding={0} />
+      <Grid id='landing-page' container columns={3} verticalAlign="middle" textAlign="center">
+        <Grid.Row>
+          <Grid.Column>
+          </Grid.Column>
+          <Grid.Column>
+            <Image centered src={cloud} size='large' />
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
         <br/>
         <Header as='h2' textAlign='center'> Make A Difference Today.
           <br/>
