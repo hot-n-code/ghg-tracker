53f464481713ab762b7bab9753811aed6351a0d5
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-03-30 17:15:59 -0700

    changing the leaf

--- app/imports/ui/components/LandingCumulativeData.jsx
+++ app/imports/ui/components/LandingCumulativeData.jsx
@@ -11,7 +11,7 @@ const LandingCumulativeData = () => (
     <div className='landing-data' style={paddingStyle}>
       <Header as='h1' textAlign='center'> <u>Malama I Ka `Aina</u></Header>
       <Header as='h2' textAlign='center'> Do your part to save Hawai&apos;i and track your carbon footprint today. </Header>
-      <Grid id='landing-page' container columns={3} verticalAlign="middle" textAlign="center">
+      <Grid id='landing-page' container columns={3} verticalAlign="middle" textAlign="center" stackable>
         <Grid.Row>
           <Grid.Column>
           </Grid.Column>
