28648529c2a90f49f7701c56980507f36f3fd2c2
Jackie Wong <wongzjackie@gmail.com>
2021-03-31 21:46:20 -1000

    Rename 'GlobalFunctions.js' file to 'vehicleDropdown.js'.

--- app/imports/ui/utilities/vehicleDropdown.js
+++ app/imports/ui/utilities/vehicleDropdown.js
@@ -1,21 +1,5 @@
 import { _ } from 'meteor/underscore';
 
-export function getAltTransportation() {
-  return ['Biking', 'Carpool', 'Public Transportation', 'Telework', 'Walking'];
-}
-
-export function computeCO2Reduced(
-  milesTraveled,
-  modeOfTransportation,
-  userVehicles,
-) {
-  const autoMPG = getAltTransportation().includes(modeOfTransportation)
-    ? _.max(userVehicles, vehicle => vehicle.MPG).MPG
-    : _.find(userVehicles, vehicle => vehicle.make === modeOfTransportation)
-        .MPG * -1;
-  return ((milesTraveled / autoMPG) * 19.6).toFixed(2);
-}
-
 export function getVehicleYearsList(model, vehicleList) {
   const filteredVehicles = vehicleList.filter(
     vehicle => vehicle.model === model,
