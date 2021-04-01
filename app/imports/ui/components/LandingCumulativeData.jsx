2522d70c9af28a8e296bd1989edb9f16e2be4c71
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-03-31 20:23:15 -0700

    fixing cloud

--- app/imports/ui/components/LandingCumulativeData.jsx
+++ app/imports/ui/components/LandingCumulativeData.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Header, Button, Image } from 'semantic-ui-react';
+import { Header, Button, Image, Grid } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 const paddingStyle = { padding: '20px' };
-
+const cloud = '../images/landing-page/cloud-trans-blank.png';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const LandingCumulativeData = () => (
     <div className='landing-data' style={paddingStyle}>
       <Header as='h1' textAlign='center'> <u>Malama I Ka `Aina</u></Header>
       <Header as='h2' textAlign='center'> Do your part to save Hawai&apos;i and track your carbon footprint today. </Header>
-      <Image centered src='/images/landing-page/cloud-trans-2.png' padding={0} />
+      <Grid columns={3}>
+          <Grid.Column>
+          </Grid.Column>
+          <Grid.Column>
+            <Image src={cloud}/>
+            <Header> 0lbs </Header>
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+      </Grid>
         <br/>
         <Header as='h2' textAlign='center'> Make A Difference Today.
           <br/>
