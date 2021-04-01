70a9489ce1ee24c81e5145b8845ef974ad8723ba
Jackie Wong <wongzjackie@gmail.com>
2021-03-31 21:31:50 -1000

    Add dynamic behavior to vehicle card comparator dropdown menus.
    - Add state variables to hold the state of the different dropdown menus and the currently selected vehicle model.
    - Change dropdown values in the 'Year' dropdown menu based on the currently selected 'Make' and 'Model'.
    - Change dropdown values in the 'Model' dropdown menu based on the currently selected 'Make'.

--- app/imports/ui/components/VehicleCard.jsx
+++ app/imports/ui/components/VehicleCard.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 import { _ } from 'meteor/underscore';
 import { Header, Button } from 'semantic-ui-react';
+import { getVehicleYearsList, getVehicle } from '../utilities/GlobalFunctions';
 import { sampleVehicles } from '../utilities/sampleData';
 
 /** Renders a single vehicle card. */
 const VehicleCard = ({ vehicle }) => {
-  // Initialize vehicle comparator's dropdown values and default vehicle.
+  // Populate vehicle comparator's dropdown values and comparator vehicle values.
   const initDropdownValues = property => {
     const sortedVehicles = _.sortBy(sampleVehicles, property);
     const listByProperty = _.pluck(sortedVehicles, property);
@@ -23,16 +24,17 @@ const VehicleCard = ({ vehicle }) => {
     return uniqueList;
   };
 
-  const initDropdownModel = currentMake => {
+  const populateDropdownModel = currentMake => {
     const filteredMakeList = _.filter(
       sampleVehicles,
       sampleVehicle => sampleVehicle.make === currentMake,
     );
     const listModel = _.pluck(filteredMakeList, 'model');
-    return listModel;
+    const uniqueModels = _.uniq(listModel);
+    return uniqueModels;
   };
 
-  const defaultComparatorVehicle = (year, model) => {
+  const getComparatorVehicle = (year, model) => {
     const yearAsInt = parseInt(year, 10);
     const listModel = _.filter(
       sampleVehicles,
@@ -44,8 +46,6 @@ const VehicleCard = ({ vehicle }) => {
     );
     return defaultVehicle;
   };
-
-  // Dropdown handlers
 
   // Destructure the 'vehicle' prop.
   const {
@@ -60,20 +60,53 @@ const VehicleCard = ({ vehicle }) => {
     type,
   } = vehicle;
 
+  // Get list of vehicle makes.
+  const makeList = initDropdownValues(Object.keys(vehicle)[1]);
+
   // State
   const [selectedId, setSelectedId] = useState(null);
   const [dropdownYear, setDropdownYear] = useState(
     initDropdownValues(Object.keys(vehicle)[6]),
   );
-  const [dropdownMake, setDropdownMake] = useState(
-    initDropdownValues(Object.keys(vehicle)[1]),
-  );
   const [dropdownModel, setDropdownModel] = useState(
-    initDropdownModel(dropdownMake[0]),
+    populateDropdownModel(makeList[0]),
   );
   const [comparatorVehicle, setComparatorVehicle] = useState(
-    defaultComparatorVehicle(dropdownYear[0], dropdownModel[0]),
+    getComparatorVehicle(dropdownYear[0], dropdownModel[0]),
   );
+  const [selectModel, setSelectModel] = useState(dropdownModel[0]);
+
+  // Dropdown handlers
+  const dropdownYearHandler = e => {
+    const yearAsInt = parseInt(e.target.value, 10);
+    const newVehicle = getVehicle(yearAsInt, selectModel, sampleVehicles);
+
+    setComparatorVehicle(newVehicle);
+  };
+
+  const dropdownMakeHandler = e => {
+    const modelList = populateDropdownModel(e.target.value);
+    const yearList = getVehicleYearsList(modelList[0], sampleVehicles);
+    const newVehicle = getVehicle(
+      parseInt(yearList[0], 10),
+      modelList[0],
+      sampleVehicles,
+    );
+
+    setComparatorVehicle(newVehicle);
+    setDropdownYear(yearList);
+    setDropdownModel(modelList);
+    setSelectModel(modelList[0]);
+  };
+
+  const dropdownModelHandler = e => {
+    const yearList = getVehicleYearsList(e.target.value, sampleVehicles);
+    const yearAsInt = parseInt(yearList[0], 10);
+    const newVehicle = getVehicle(yearAsInt, e.target.value, sampleVehicles);
+    setSelectModel(e.target.value);
+    setDropdownYear(yearList);
+    setComparatorVehicle(newVehicle);
+  };
 
   return (
     <AnimateSharedLayout type='crossfade'>
@@ -208,7 +241,10 @@ const VehicleCard = ({ vehicle }) => {
                       </motion.div>
                       <motion.div className='vehicle-card-expand-header'>
                         <Header as='h1'>
-                          <select className='vehicle-card-dropdown'>
+                          <select
+                            className='vehicle-card-dropdown'
+                            onChange={dropdownYearHandler}
+                          >
                             {dropdownYear.map((yearOption, key) => (
                               <option
                                 className='vehicle-card-dropdown-option'
@@ -219,8 +255,11 @@ const VehicleCard = ({ vehicle }) => {
                               </option>
                             ))}
                           </select>
-                          <select className='vehicle-card-dropdown'>
-                            {dropdownMake.map((makeOption, key) => (
+                          <select
+                            className='vehicle-card-dropdown'
+                            onChange={dropdownMakeHandler}
+                          >
+                            {makeList.map((makeOption, key) => (
                               <option
                                 className='vehicle-card-dropdown-option'
                                 value={makeOption}
@@ -230,7 +269,10 @@ const VehicleCard = ({ vehicle }) => {
                               </option>
                             ))}
                           </select>
-                          <select className='vehicle-card-dropdown'>
+                          <select
+                            className='vehicle-card-dropdown'
+                            onChange={dropdownModelHandler}
+                          >
                             {dropdownModel.map((modelOption, key) => (
                               <option
                                 className='vehicle-card-dropdown-option'
