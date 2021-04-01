a13087645f50410243d701b96466d1bf63603cb6
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-03-31 21:36:11 -0700

    updated text

--- app/imports/ui/components/LandingCumulativeData.jsx
+++ app/imports/ui/components/LandingCumulativeData.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import { Header, Button, Image } from 'semantic-ui-react';
+import { Header, Button, Image, Grid } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
+import { _ } from 'meteor/underscore';
+import { DailyUserData } from '../../api/ghg-data/DailyUserDataCollection';
 
 const paddingStyle = { padding: '20px' };
-
+const cloud = '../images/landing-page/cloud-trans-blank.png';
+const ghg = _.pluck(DailyUserData.collection.find({}).fetch(), 'cO2Reduced');
+const ghgTotal = _.reduce(ghg, function (sum, num) { return sum + num; }, 0);
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const LandingCumulativeData = () => (
     <div className='landing-data' style={paddingStyle}>
       <Header as='h1' textAlign='center'> <u>Malama I Ka `Aina</u></Header>
       <Header as='h2' textAlign='center'> Do your part to save Hawai&apos;i and track your carbon footprint today. </Header>
-      <Image centered src='/images/landing-page/cloud-trans-2.png' padding={0} />
+      <Grid columns={3}>
+        <Grid.Column>
+        </Grid.Column>
+        <Grid.Column>
+          <div>
+            <Image src={cloud}/>
+            <Header as='h2' id='cloud-carbon'> {ghgTotal} LBS.</Header>
+          </div>
+        </Grid.Column>
+        <Grid.Column>
+        </Grid.Column>
+      </Grid>
+      <br/>
+      <Header as='h2' textAlign='center'> Make A Difference Today.
         <br/>
-        <Header as='h2' textAlign='center'> Make A Difference Today.
-          <br/>
-          <div style={{ paddingTop: '16px' }}></div>
+        <div style={{ paddingTop: '16px' }}></div>
         <Button className="ui massive blue button" position='centered' as={NavLink} activeClassName="active" exact to="/signup">Sign Up</Button>
-        </Header>
+      </Header>
     </div>
-    );
+);
 
 export default LandingCumulativeData;
