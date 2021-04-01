db75c88514120c49ff1e1d4666771a2a3cc2aa40
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2021-03-31 20:52:18 -0700

    clouds

--- app/imports/ui/components/LandingCumulativeData.jsx
+++ app/imports/ui/components/LandingCumulativeData.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Header, Button, Image } from 'semantic-ui-react';
+import { Header, Button, Image, Grid } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { _ } from 'meteor/underscore';
 import { DailyUserData } from '../../api/ghg-data/DailyUserDataCollection';
 
+const cloud = '/images/landing-page/cloud-trans.png';
 const paddingStyle = { padding: '20px' };
 const ghg = _.pluck(DailyUserData.collection.find({}).fetch(), 'cO2Reduced');
 const ghgTotal = _.reduce(ghg, function (sum, num) { return sum + num; }, 0);
@@ -12,8 +13,18 @@ const LandingCumulativeData = () => (
     <div className='landing-data' style={paddingStyle}>
       <Header as='h1' textAlign='center'> <u>Malama I Ka `Aina</u></Header>
       <Header as='h2' textAlign='center'> Do your part to save Hawai&apos;i and track your carbon footprint today. </Header>
-      <Image centered src='/images/landing-page/cloud-trans.png' padding={0} />
-      <Header as='h2' id='cloud-carbon'> {ghgTotal} LBS.</Header>
+      <Grid id='landing-page' container columns={3} verticalAlign="middle" textAlign="center" stackable>
+        <Grid.Row>
+          <Grid.Column>
+          </Grid.Column>
+          <Grid.Column>
+            <Image centered src={cloud} size='large' />
+            <Header as='h2' id='cloud-carbon'> {ghgTotal} LBS.</Header>
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
         <br/>
         <Header as='h2' textAlign='center'> Make A Difference Today.
           <br/>
