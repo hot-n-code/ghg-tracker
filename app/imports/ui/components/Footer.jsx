b52ea7c5eaca8b4b858c458863c86dd793ba4bc8
Your Name <yiwenc22@gmail.com>
2021-03-31 15:05:05 -1000

    Enhance footer to make more sense
    Before user can still go back to sign in page and join page when have login in the footer. Now, I added bound just like the nav bar when user log in the footer will change to users footer and when log out it go back to non user footer.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Grid, GridColumn, Image, List } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { withRouter, NavLink } from 'react-router-dom';
+import { Dropdown, Grid, GridColumn, Image, List, Menu } from 'semantic-ui-react';
+import { Roles } from 'meteor/alanning:roles';
 
 const logo = '../images/hei.png';
 
@@ -13,17 +18,27 @@ class Footer extends React.Component {
               <div className="ui container">
                 <Grid columns={2}>
                   <GridColumn className="inverted">
-                    <hr />
-                    <List inverted>
-                      <List.Item href='/#/' className="footer-text">Home</List.Item>
-                      <List.Item href='/#/cumulative' className="footer-text">Cumulative Data</List.Item>
-                      <List.Item href='/#/about' className="footer-text">About HEI</List.Item>
-                      <List.Item href='/signup#/signup' className="footer-text">Join</List.Item>
-                      <List.Item href='/#/signin' className="footer-text">Sign In</List.Item>
-                      <List.Item href='/#/feedback' className="foot-text">Submit Feedback</List.Item>
-                    </List>
+                      {this.props.currentUser === '' ? (
+                          <List inverted>
+                            <List.Item href='/' className="footer-text">Home</List.Item>
+                            <List.Item href='/cumulative' className="footer-text">Cumulative Data</List.Item>
+                            <List.Item href='/about' className="footer-text">About HEI</List.Item>
+                            <List.Item href='/signup#/signup' className="footer-text">Join</List.Item>
+                            <List.Item href='/signin' className="footer-text">Sign In</List.Item>
+                            <List.Item href='/feedback' className="foot-text">Submit Feedback</List.Item>
+                          </List>
+                      ) : (
+                          <List inverted>
+                            <List.Item href='/' className="footer-text">Home</List.Item>
+                            <List.Item href='/user-page' className="foot-text">My Data</List.Item>
+                            <List.Item href='/my-vehicles' className="foot-text">My Vehicles</List.Item>
+                            <List.Item href='/alt' className="foot-text">Alternative</List.Item>
+                            <List.Item href='/cumulative' className="footer-text">Cumulative Data</List.Item>
+                            <List.Item href='/user-data-page' className="footer-text">Transportation History</List.Item>
+                            <List.Item href='/feedback' className="foot-text">Submit Feedback</List.Item>
+                          </List>
+                      )}
                   </GridColumn>
-
                   <GridColumn>
                     <hr />
                     <List inverted>
@@ -45,4 +60,14 @@ class Footer extends React.Component {
   }
 }
 
-export default Footer;
+/** Declare the types of all properties. */
+Footer.propTypes = {
+  currentUser: PropTypes.string,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+const FooterContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(Footer);
+
+export default withRouter(FooterContainer);
