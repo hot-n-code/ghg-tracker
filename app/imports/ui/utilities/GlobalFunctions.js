07dc1dcca24d13d8b349868cdd1dc545ecfd93a4
Jackie Wong <wongzjackie@gmail.com>
2021-03-31 21:28:35 -1000

    Update and add global functions.
    - Update 'getVehicleYearsList' function to be reusable in other instances.
    - Add 'getVehicle' function to retrieve the newly selected comparator vehicle.

--- app/imports/ui/utilities/GlobalFunctions.js
+++ app/imports/ui/utilities/GlobalFunctions.js
@@ -15,3 +15,26 @@ export function computeCO2Reduced(
         .MPG * -1;
   return ((milesTraveled / autoMPG) * 19.6).toFixed(2);
 }
+
+export function getVehicleYearsList(model, vehicleList) {
+  const filteredVehicles = vehicleList.filter(
+    vehicle => vehicle.model === model,
+  );
+  const listYear = _.pluck(filteredVehicles, 'year');
+  const listUniqueYear = _.uniq(listYear);
+  const reverseList = _.sortBy(listUniqueYear, year => year * -1);
+
+  for (let i = 0; i < reverseList.length; i++) {
+    reverseList[i] = reverseList[i].toString();
+  }
+
+  return reverseList;
+}
+
+export function getVehicle(year, model, vehicleList) {
+  const filterByModel = vehicleList.filter(vehicle => vehicle.model === model);
+  const vehicle = filterByModel.find(
+    selectVehicle => selectVehicle.year === year,
+  );
+  return vehicle;
+}
