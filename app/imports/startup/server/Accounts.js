5ae43b03aa665820c7950c7f7bb7c782943eada2
Daphne Tapia <dmtapia@hawaii.edu>
2021-04-02 21:48:47 -1000

    defaultAccounts moved

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -19,10 +19,11 @@ function createUser(email, password, role) {
 
 /** When running app for first time, pass a settings file to set up a default user account. */
 if (Meteor.users.find().count() === 0) {
-  if (Meteor.settings.defaultAccounts) {
-    console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+  if (Meteor.isServer) {
+    const defaultAccounts = JSON.parse(Assets.getText('defaultAccounts.json'));
+    console.log('Creating the default accounts(s)');
+    defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
   } else {
-    console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
+    console.log('Cannot initialize the database! Make sure Meteor is running in server environment');
   }
 }
