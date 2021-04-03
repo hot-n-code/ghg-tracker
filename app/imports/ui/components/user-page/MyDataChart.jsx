897fb0389bc4d7f9219d7db4e29d8ce4e7813e3d
Sophia Elize Cruz <secruz@hawaii.edu>
2021-04-02 20:51:57 -1000

    implemented suggestions from code review

--- app/imports/ui/components/user-page/MyDataChart.jsx
+++ app/imports/ui/components/user-page/MyDataChart.jsx
@@ -7,31 +7,44 @@ import { DailyUserData } from '../../../api/user/ghg-data/DailyUserDataCollectio
 import { getCumulativePerMode } from '../../utilities/CumulativeGHGData';
 import PropTypes from 'prop-types';
 
-// Pie chart of the mileage for each mode of transportation for a specific user
+// Pie chart of the all time mileage for each mode of transportation for a specific user
+
+const graphObject = {
+    telework: 'Telework',
+    pTransportation: 'Public Transportation',
+    biking: 'Biking',
+    walking: 'Walking',
+    carpool: 'Carpool',
+    evHybrid: 'EVHybrid',
+    transportationTypes: ['Telework', 'Public Transportation', 'Biking',
+        'Walk', 'Carpool', 'EVHybrid'],
+    graphColors: ['#5c8d89', '#4b8796', '#4f7fa0', '#6872a0', '#846391', '#985575'],
+};
+
 const MyDataChart = (props) => {
-    const teleworkData = getCumulativePerMode(props.userData, 'Telework');
+    const teleworkData = getCumulativePerMode(props.userData, graphObject.telework);
     const totalTelework = teleworkData.VMTReduced;
-    const aVData = getCumulativePerMode(props.userData, 'EV/Hybrid');
+    const aVData = getCumulativePerMode(props.userData, graphObject.evHybrid);
     const totalAV = aVData.VMTReduced;
-    const bikingData = getCumulativePerMode(props.userData, 'Biking');
+    const bikingData = getCumulativePerMode(props.userData, graphObject.biking);
     const totalBiking = bikingData.VMTReduced;
-    const walkingData = getCumulativePerMode(props.userData, 'Walking');
+    const walkingData = getCumulativePerMode(props.userData, graphObject.walking);
     const totalWalking = walkingData.VMTReduced;
-    const carpoolData = getCumulativePerMode(props.userData, 'Carpool');
+    const carpoolData = getCumulativePerMode(props.userData, graphObject.carpool);
     const totalCarpool = carpoolData.VMTReduced;
-    const ptData = getCumulativePerMode(props.userData, 'Public Transportation');
+    const ptData = getCumulativePerMode(props.userData, graphObject.pTransportation);
     const totalPT = ptData.VMTReduced;
     const stateAll = {
-        labels: ['Telework', 'Public Transportation', 'Biking', 'Walk', 'Carpool', 'EV/Hybrid'],
+        labels: graphObject.transportationTypes,
         datasets: [{ data: [totalTelework, totalPT, totalBiking, totalWalking, totalCarpool, totalAV],
-            backgroundColor: ['#5c8d89', '#4b8796', '#4f7fa0', '#6872a0', '#846391', '#985575'],
+            backgroundColor: graphObject.graphColors,
         }],
     };
 
     return (
         <Grid>
             <Grid.Column>
-                <Pie data={stateAll} height='250px'/>
+                <Pie data={stateAll} height={250}/>
             </Grid.Column>
         </Grid>
     );
