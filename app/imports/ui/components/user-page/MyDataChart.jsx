1e765f798440bb84894e945f71cdbd1d84d13258
Sophia Elize Cruz <secruz@hawaii.edu>
2021-04-02 20:37:17 -1000

    implementing code review changes

--- app/imports/ui/components/user-page/MyDataChart.jsx
+++ app/imports/ui/components/user-page/MyDataChart.jsx
@@ -1,48 +1,52 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Grid } from 'semantic-ui-react';
-import { _ } from 'meteor/underscore';
 import { Pie } from 'react-chartjs-2';
-import { withRouter } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
 import { DailyUserData } from '../../../api/user/ghg-data/DailyUserDataCollection';
+import { getCumulativePerMode } from '../../utilities/CumulativeGHGData';
+import PropTypes from 'prop-types';
 
-// Displaying a pie chart of the mode of transportation from DailyUserData collection
-const MyDataChart = () => {
-    const email = Meteor.user().username;
-    const totalTelework = _.reduce(_.pluck(DailyUserData.collection.find({ owner: email,
-        modeOfTransportation: 'Telework' }).fetch(), 'milesTraveled'), (total, num) => total + num, 0);
-    const totalAV = _.reduce(_.pluck(DailyUserData.collection.find({ owner: email,
-        modeOfTransportation: 'Alternative Fuel Vehicle' }).fetch(), 'milesTraveled'), (total, num) => total + num, 0);
-    const totalBiking = _.reduce(_.pluck(DailyUserData.collection.find({ owner: email,
-        modeOfTransportation: 'Biking' }).fetch(), 'milesTraveled'), (total, num) => total + num, 0);
-    const totalWalking = _.reduce(_.pluck(DailyUserData.collection.find({ owner: email,
-        modeOfTransportation: 'Walking' }).fetch(), 'milesTraveled'), (total, num) => total + num, 0);
-    const totalCarpool = _.reduce(_.pluck(DailyUserData.collection.find({ owner: email,
-        modeOfTransportation: 'Carpool' }).fetch(), 'milesTraveled'), (total, num) => total + num, 0);
-    const totalPT = _.reduce(_.pluck(DailyUserData.collection.find({ owner: email,
-        modeOfTransportation: 'Public Transportation' }).fetch(), 'milesTraveled'), (total, num) => total + num, 0);
-    const totalCar = _.where(DailyUserData.collection.find({}).fetch(), { owner: email });
-    let gasTotal = 0;
-    totalCar.forEach((obj) => {
-           if (obj.cO2Reduced < 0) {
-               gasTotal += obj.milesTraveled;
-           }
-        });
-    // console.log(gasTotal);
+// Pie chart of the mileage for each mode of transportation for a specific user
+const MyDataChart = (props) => {
+    const teleworkData = getCumulativePerMode(props.userData, 'Telework');
+    const totalTelework = teleworkData.VMTReduced;
+    const aVData = getCumulativePerMode(props.userData, 'EV/Hybrid');
+    const totalAV = aVData.VMTReduced;
+    const bikingData = getCumulativePerMode(props.userData, 'Biking');
+    const totalBiking = bikingData.VMTReduced;
+    const walkingData = getCumulativePerMode(props.userData, 'Walking');
+    const totalWalking = walkingData.VMTReduced;
+    const carpoolData = getCumulativePerMode(props.userData, 'Carpool');
+    const totalCarpool = carpoolData.VMTReduced;
+    const ptData = getCumulativePerMode(props.userData, 'Public Transportation');
+    const totalPT = ptData.VMTReduced;
     const stateAll = {
-        labels: ['Telework', 'Public Transportation', 'Biking', 'Walk', 'Carpool', 'EV/Hybrid', 'Gas'],
-        datasets: [{ data: [totalTelework, totalPT, totalBiking, totalWalking, totalCarpool, totalAV, gasTotal],
-            backgroundColor: ['#5c8d89', '#4b8796', '#4f7fa0', '#6872a0', '#846391', '#985575', '#de0d0d'],
+        labels: ['Telework', 'Public Transportation', 'Biking', 'Walk', 'Carpool', 'EV/Hybrid'],
+        datasets: [{ data: [totalTelework, totalPT, totalBiking, totalWalking, totalCarpool, totalAV],
+            backgroundColor: ['#5c8d89', '#4b8796', '#4f7fa0', '#6872a0', '#846391', '#985575'],
         }],
     };
 
     return (
         <Grid>
             <Grid.Column>
-                <Pie data={stateAll} height={250}/>
+                <Pie data={stateAll} height='250px'/>
             </Grid.Column>
         </Grid>
     );
 };
 
-export default withRouter(MyDataChart);
+MyDataChart.propTypes = {
+    userData: PropTypes.array.isRequired,
+};
+
+export default withTracker(() => {
+    const subscription1 = Meteor.subscribe(DailyUserData.userPublicationName);
+    const ready = subscription1.ready();
+    const userData = DailyUserData.collection.find({}).fetch();
+    return {
+        userData,
+        ready,
+    };
+})(MyDataChart);
