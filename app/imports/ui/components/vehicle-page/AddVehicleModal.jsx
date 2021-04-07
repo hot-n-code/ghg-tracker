89354031f9f44265c68036184e07c767f1077e6d
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-04-07 08:42:23 -1000

    create form

--- app/imports/ui/components/vehicle-page/AddVehicleModal.jsx
+++ app/imports/ui/components/vehicle-page/AddVehicleModal.jsx
@@ -38,9 +38,22 @@ const AddVehicleModal = (props) => {
     return year;
   };
   const [test, setTest] = useState(false);
-  /** On submit, insert the data. */
-  function submit(data, formRef) {
-    const { make, model, price, year, fuelSpending } = data;
+  const [dropMake] = useState(['Toyota', 'Honda', 'Nissan', 'Tesla', 'Ford', 'Volkswagen']);
+  const [dropModel, setDropModel] = useState(() => populateCar('Honda'));
+  const [dropYear, setDropYear] = useState(() => ['2020']);
+  const [finalMake, setFinalMake] = useState(() => '');
+  const [finalModel, setFinalModel] = useState(() => '');
+  const [finalYear, setFinalYear] = useState(() => '');
+  const [finalPrice, setFinalPrice] = useState(() => '');
+  const [finalSpending, setFinalSpending] = useState(() => '');
+  const handleSubmit = () => {
+    const make = finalMake;
+    const model = finalModel;
+    const price = finalPrice;
+    const year = finalYear;
+    const fuelSpending = finalSpending;
+    setFinalPrice('');
+    setFinalSpending('');
     const owner = Meteor.user().username;
     // LOGO
     const temp = _.pluck(Makes.collection.find({ make: make }).fetch(), 'logo');
@@ -50,34 +63,30 @@ const AddVehicleModal = (props) => {
     const MPG = get[0];
     const type = get[1];
     UserVehicle.collection.insert(
-      { make, model, logo, price, year, MPG, fuelSpending, type, owner },
-      error => {
-        if (error) {
-          swal('Error', error.message, 'error');
-        } else {
-          swal('Success', 'Vehicle added successfully', 'success');
-          formRef.reset();
-        }
-      },
+        { make, model, logo, price, year, MPG, fuelSpending, type, owner },
+        error => {
+          if (error) {
+            swal('Error', error.message, 'error');
+          } else {
+            swal('Success', 'Vehicle added successfully', 'success');
+          }
+        },
     );
-  }
-    // const xd = populateCar('Honda');
-  const [dropMake, setDropMake] = useState(['Toyota', 'Honda', 'Nissan', 'Tesla', 'Ford', 'Volkswagen']);
-  const [dropModel, setDropModel] = useState(() => populateCar('Honda'));
-  const [dropYear, setDropYear] = useState(() => ['2020']);
-  const [finalMake, setFinalMake] = useState(() => '');
-  const [finalModel, setFinalModel] = useState(() => '');
-  const [finalYear, setFinalYear] = useState(() => '');
+  };
   const changeModel = e => {
     setFinalMake(e.target.value);
     setDropModel(populateCar(e.target.value));
   };
   const changeYear = e => {
     setFinalModel(e.target.value);
-    setDropYear(populateYear(finalMake, e.target.value));
+    const desc = _.sortBy(populateYear(finalMake, e.target.value), function (num) {
+        return num;
+    });
+    setDropYear(desc.reverse());
   };
   const endYear = e => {
-    setFinalYear(e.target.value);
+    const yearAsInt = parseInt(e.target.value, 10);
+    setFinalYear(yearAsInt);
   };
     // Animation variants
     const overlay = {
@@ -136,27 +145,52 @@ const AddVehicleModal = (props) => {
                   className='add-vehicle-form'
                   layoutId='add-vehicle-toggle'
                 >
-                  <select onChange={changeModel}>
-                    {dropMake.map((make, key) => (
-                      <option value={make} key={key}>
-                        {make}
-                      </option>
-                        ))}
-                  </select>
-                  <select onChange={changeYear}>
-                    {dropModel.map((model, key) => (
-                        <option value={model} key={key}>
-                          {model}
-                        </option>
-                    ))}
-                  </select>
-                  <select onChange={endYear}>
-                    {dropYear.map((year, key) => (
-                        <option value={year} key={key}>
-                          {year}
-                        </option>
-                    ))}
-                  </select>
+                 <form onSubmit={handleSubmit}>
+                   <label>
+                     Make:
+                     <select onChange={changeModel}>
+                       {dropMake.map((make, key) => (
+                           <option value={make} key={key}>
+                             {make}
+                           </option>
+                       ))}
+                     </select>
+                   </label>
+                   <br/>
+                   <label>
+                     Model:
+                     <select onChange={changeYear}>
+                       {dropModel.map((model, key) => (
+                           <option value={model} key={key}>
+                             {model}
+                           </option>
+                       ))}
+                     </select>
+                   </label>
+                   <br/>
+                   <label>
+                     Year:
+                     <select onChange={endYear}>
+                       {dropYear.map((year, key) => (
+                           <option value={year} key={key}>
+                             {year}
+                           </option>
+                       ))}
+                     </select>
+                   </label>
+                   <br/>
+                   <label>
+                     Price:
+                     <input type='text' value={finalPrice} onChange={e => setFinalPrice(e.target.value)} />
+                   </label>
+                   <br/>
+                   <label>
+                     Yearly Spending:
+                     <input type='text' value={finalSpending} onChange={e => setFinalSpending(e.target.value)} />
+                   </label>
+                   <br/>
+                   <input type='submit' value='Submit'/>
+                 </form>
                 </motion.div>
               </motion.div>
             </motion.div>
