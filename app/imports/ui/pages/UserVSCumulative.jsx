702232a1bf9d254446963bdf92d8338eb6c563ba
Sophia Elize Cruz <secruz@hawaii.edu>
2021-04-11 00:53:28 -1000

    cumulative page for user v cumulative

--- app/imports/ui/pages/UserVSCumulative.jsx
+++ app/imports/ui/pages/UserVSCumulative.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Container, Header } from 'semantic-ui-react';
+import { Grid, Container, Header, Card, Image, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { _ } from 'meteor/underscore';
 import { Users } from '../../api/user/UserCollection';
 import { DailyUserData } from '../../api/user/ghg-data/DailyUserDataCollection';
-import WhatIf from '../components/user-data-page/WhatIf';
 import ComparisonGraph from '../components/ComparisonGraph';
 import { getCumulativeGHG } from '../utilities/CumulativeGHGData';
 import { UserVehicle } from '../../api/user/UserVehicleCollection';
 
 
 const paddingStyle = { padding: 20 };
+const rideStyle = { height: '200px', width: '300px' };
 
 const UserVSCumulative = (props) => {
     const date = new Date();
@@ -23,39 +23,14 @@ const UserVSCumulative = (props) => {
             date.getMonth() && userTrip.inputDate.getFullYear() === date.getFullYear());
     });
     const thisMonthGHGData = getCumulativeGHG(getThisMonth, props.vehicles);
+    const thisMonthCO2Produced = thisMonthGHGData.cO2Produced;
     const thisMonthCO2Reduced = thisMonthGHGData.cO2Reduced;
 
-    // const getLastMonth = _.filter(props.dailyData, (userTrip) => {
-    //     if (date.getMonth() - 1 !== -1) {
-    //         return (userTrip.inputDate.getMonth() - 1 ===
-    //             date.getMonth() - 1 && userTrip.inputDate.getFullYear() === date.getFullYear());
-    //     } else {
-    //         return (userTrip.inputDate.getMonth() - 1 ===
-    //             date.getMonth() - 1 && userTrip.inputDate.getFullYear() - 1 === date.getFullYear() - 1);
-    //     }
-    // });
-
-    // const lastMonthsGHGData = getCumulativeGHG(getLastMonth, props.allVehicles);
-    // const lastMonthCO2Produced = lastMonthsGHGData.cO2Produced;
-
-    // const increase = thisMonthCO2Produced - lastMonthCO2Produced !== 0 ? thisMonthCO2Produced - lastMonthCO2Produced : '';
-    // let result;
-    // if (increase > 0 && lastMonthCO2Produced !== 0) {
-    //     result = 'Your CO2 Production is up ' + ((increase / lastMonthCO2Produced) * 100).toFixed(2)
-    //         + '% from last month!';
-    // } else if (increase < 0 && lastMonthCO2Produced !== 0) {
-    //     result = 'Your CO2 Production is down ' +
-    //         (((lastMonthCO2Produced - thisMonthCO2Produced) / lastMonthCO2Produced) * 100).toFixed(2) +
-    //         '% from last month. Keep up the good work!';
-    // } else if (lastMonthCO2Produced === 0 && increase > 0) {
-    //     result = increase;
-    // } else {
-    //     result = 'No CO2 Production data available for this user.';
-    // }
     let result;
-    if (thisMonthCO2Reduced < 0) {
-            result = 'Uh-oh. Looks like your CO2 production has gone up. Think you can do better?';
-        } else if (thisMonthCO2Reduced > 0 ){
+    if (thisMonthCO2Produced > 0 && (thisMonthCO2Reduced === 0 || thisMonthCO2Reduced < thisMonthCO2Produced)) {
+            result = 'Uh-oh. Looks like you`&apos;`re producing more emissions rather than reducing them.' +
+                'Maybe consider a form of alternative transportation?';
+        } else if (thisMonthCO2Reduced > thisMonthCO2Produced){
             result = 'Your CO2 reduction efforts are paying off! You have reduced ' + thisMonthCO2Reduced +
                 ' lbs of CO2. Keep up the good work!';
         } else {
@@ -74,8 +49,96 @@ const UserVSCumulative = (props) => {
             <Grid stackable>
                 <Grid.Column textAlign='center' width={16}>
                     <Header as='h1'>{result}</Header>
-                    <Header as='h1'>Want more alternative transportation options?</Header>
-                    <WhatIf/>
+                    <Header as='h1'>Think you can do better?
+                        Try some of your local alternative transportation options!</Header>
+                    <Header as='h1' textAlign='center'>Oahu</Header>
+                    <Card.Group style={{ display: 'flex', justifyContent: 'center', paddingTop: '20px',
+                        paddingBottom: '20px' }}>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>Biki</Card.Header>
+                                <Card size='medium' color='teal'>
+                                    <Image style={rideStyle}
+                                     src='https://kitv.images.worldnow.com/images/17721408_G.jpg?auto=webp&disable=
+                                    upscale&height=560&fit=bounds&lastEditedDate=1539244048000'/>
+                                </Card>
+                                <Button href='https://gobiki.org/map-of-biki-stops' target='_blank'>
+                                    Learn More
+                                </Button>
+                            </Card.Content>
+                        </Card>
+                        <Card>
+                          <Card.Content>
+                            <Card.Header>The Bus</Card.Header>
+                              <Card size='medium' color='teal'>
+                                  <Image style={rideStyle}
+                                     src='https://hawaii-com-wp.s3.us-west-1.amazonaws.com/wp-content/uploads/2004/12/08100000/thebus.jpg'
+                                  />
+                              </Card>
+                              <Button href='http://hea.thebus.org/' target='_blank'>
+                                  Learn More
+                              </Button>
+                            </Card.Content>
+                        </Card>
+                        <Card>
+                          <Card.Content>
+                            <Card.Header>HUI Car Share</Card.Header>
+                              <Card size='medium' color='teal'>
+                                  <Image style={rideStyle}
+                                         src='https://mediad.publicbroadcasting.net/p/khpr/files/styles/x_large/public/201807/IMG_5759-2.jpg'
+                                  />
+                              </Card>
+                              <Button href='https://www.drivehui.com/' target='_blank'>
+                                  Learn More
+                              </Button>
+                            </Card.Content>
+                        </Card>
+                    </Card.Group>
+                    <Header as='h1' textAlign='center'>Neighbor Islands</Header>
+                    <Card.Group style={{ display: 'flex', justifyContent: 'center', paddingTop: '20px' }}>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>Hele-On (Big Island)</Card.Header>
+                                <Card size='medium' color='teal'>
+                                    <Image style={rideStyle}
+                                           src='https://hilo.hawaii.edu/campuslife/images/campuslife/HELEON_383x362.jpg'
+                                    />
+                                </Card>
+                                <Button href='http://www.heleonbus.org/schedules-and-maps/general-information'
+                                        target='_blank'>
+                                    Learn More
+                                </Button>
+                            </Card.Content>
+                        </Card>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>Maui Bus (Maui)</Card.Header>
+                                <Card size='medium' color='teal'>
+                                    <Image style={rideStyle}
+                                           src='https://airports.hawaii.gov/ogg/wp-content/uploads/sites/7/2016/07/MauiBus.jpg'
+                                    />
+                                </Card>
+                                <Button href='https://www.mauibus.org/map'
+                                        target='_blank'>
+                                    Learn More
+                                </Button>
+                            </Card.Content>
+                        </Card>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>The Kaua&apos;i Bus (Kauai)</Card.Header>
+                                <Card size='medium' color='teal'>
+                                    <Image style={rideStyle}
+                                           src='https://www.thegardenisland.com/wp-content/uploads/2017/06/7bb6da5b281a42d51a9dc1d9ac37709f.jpg'
+                                    />
+                                </Card>
+                                <Button href='https://www.kauai.com/kauai-bus'
+                                        target='_blank'>
+                                    Learn More
+                                </Button>
+                            </Card.Content>
+                        </Card>
+                    </Card.Group>
                 </Grid.Column>
             </Grid>
         </Container>
