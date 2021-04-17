0ac57065bc33feb146c2a353027eb10543e24ad9
Sophia Elize Cruz <secruz@hawaii.edu>
2021-04-16 20:11:13 -1000

    beginning ui edits to comparison page

--- app/imports/ui/pages/user/UserVSCumulative.jsx
+++ app/imports/ui/pages/user/UserVSCumulative.jsx
@@ -33,8 +33,8 @@ class UserVSCumulative extends React.Component {
             result = 'Uh-oh. It looks like you are producing more emissions rather than reducing them.' +
                 ' Maybe consider a form of alternative transportation?';
         } else if (thisMonthCO2Reduced > thisMonthCO2Produced) {
-            result = `Your CO2 reduction efforts are paying off! You have reduced ${thisMonthCO2Reduced.toFixed(2)
-                } lbs of CO2. Keep up the good work!`;
+            result = `Your CO2 reduction efforts are paying off! So far, you have 
+            reduced ${thisMonthCO2Reduced.toFixed(2)} lbs of CO2. Keep up the good work!`;
         } else {
             result = 'No pounds of CO2 reduced available for this month. Start adding in your trips!';
         }
