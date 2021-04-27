f16563ffedb7e964ddd8078d26a7731bed4d0f81
vrabang <boque.rabang@gmail.com>
2021-04-26 22:32:58 -1000

    fixed eslint

--- app/imports/ui/pages/admin/AdminProfileList.jsx
+++ app/imports/ui/pages/admin/AdminProfileList.jsx
@@ -4,8 +4,6 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import SmartDataTable from 'react-smart-data-table';
 import { Users } from '../../../api/user/UserCollection';
-import { UserVehicles } from '../../../api/user/UserVehicleCollection';
-// import { getVehicle } from '../.../utilities/vehicleDropdown.js';
 import 'react-smart-data-table/dist/react-smart-data-table.css';
 import DeleteUser from '../../components/admin-users-page/DeleteUser';
 
